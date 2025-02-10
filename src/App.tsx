94bf006d02a125346779b84a7e6e774f946a8655
Xin Qiao <qiaoxin136@gmail.com>
2025-02-10 12:35:52 -0500

    add map 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 //import { AmplifyMapLibreRequest } from 'maplibre-gl-js-amplify';
 
-import { MapView } from '@aws-amplify/ui-react-geo';
+import { MapView} from '@aws-amplify/ui-react-geo';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 const INITIAL_VIEW_STATE = {
-  longitude: 139.7674681227469,
-  latitude: 35.68111419325676,
-  zoom: 11,
+  //longitude: 139.7674681227469,
+  longitude: -80.20321, 
+  //latitude: 35.68111419325676,
+  latitude: 26.00068, 
+  zoom: 17,
   bearing: 0,
   pitch: 0,
 };

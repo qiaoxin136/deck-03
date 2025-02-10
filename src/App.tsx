8085a939a074959ed27a8d6824ad80a5dc022c29
Xin Qiao <qiaoxin136@gmail.com>
2025-02-10 12:25:12 -0500

    add map 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,23 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 //import { AmplifyMapLibreRequest } from 'maplibre-gl-js-amplify';
 
-import "maplibre-gl/dist/maplibre-gl.css";
+import { MapView } from '@aws-amplify/ui-react-geo';
+import 'maplibre-gl/dist/maplibre-gl.css';
 
-import { useEffect } from "react";
-import { createMap } from "maplibre-gl-js-amplify";
-// 以下だけ変える必要がある。Amplify -> {Amplify}
-
-import "maplibre-gl/dist/maplibre-gl.css";
-
+const INITIAL_VIEW_STATE = {
+  longitude: 139.7674681227469,
+  latitude: 35.68111419325676,
+  zoom: 11,
+  bearing: 0,
+  pitch: 0,
+};
 //const client = generateClient<Schema>();
 
 function App() {
   //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const { signOut } = useAuthenticator();
 
-  async function initializeMap() {
-    await createMap({
-      container: 'map', // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js/docs/API/classes/Map/
-      style: 'AmplifyMapLibreRequest',
-      center: [-123.1187, 49.2819], // [Longitude, Latitude]
-      zoom: 11
-    });
-  }
-
-
-  useEffect(() => {
-    initializeMap();
-  }, []);
-
+  
   // useEffect(() => {
   //   client.models.Todo.observeQuery().subscribe({
   //     next: (data) => setTodos([...data.items]),
@@ -48,7 +37,7 @@ function App() {
 
   return (
     <main>
-      <div id="map" style={{ height: "100vh" }} />
+      return <MapView initialViewState={INITIAL_VIEW_STATE} />;
       <button onClick={signOut}>Sign out</button>
     </main>
   );

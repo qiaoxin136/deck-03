8375addbfb5b30b9eaa097d82966c0518f05c220
Xin Qiao <qiaoxin136@gmail.com>
2025-02-11 13:54:39 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { DeckGL } from '@deck.gl/react';
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { MapView } from "@aws-amplify/ui-react-geo";
-import { MapViewState } from '@deck.gl/react';
+//import { MapViewState } from '@deck.gl/react';
 import { Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import "maplibre-gl/dist/maplibre-gl.css";
 
 
-const INITIAL_VIEW_STATE: MapViewState = {
+const INITIAL_VIEW_STATE: any = {
   //longitude: 139.7674681227469,
   longitude: -80.20321,
   //latitude: 35.68111419325676,
@@ -20,7 +20,7 @@ const INITIAL_VIEW_STATE: MapViewState = {
 
 function App() {
   const { signOut } = useAuthenticator();
-  const [viewport, setViewport] = useState<MapViewState>(INITIAL_VIEW_STATE);
+  const [viewport, setViewport] = useState(INITIAL_VIEW_STATE);
   const layers:any = [];
 
   return (
@@ -31,7 +31,7 @@ function App() {
           initialViewState={INITIAL_VIEW_STATE}
           controller
           layers={layers}
-          onViewStateChange={({ viewState }: { viewState: MapViewState }) =>
+          onViewStateChange={({ viewState }) =>
             setViewport(viewState)
           }
         >

c6a8488c4d02c75e19e7ff063af5cec87d0f125d
Xin Qiao <qiaoxin136@gmail.com>
2025-02-11 13:08:00 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,52 @@
-//import {  useState } from "react";
-//import type { Schema } from "../amplify/data/resource";
-//import { generateClient } from "aws-amplify/data";
+import { useState } from "react";
+import { DeckGL } from "deck.gl/src";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-//import { AmplifyMapLibreRequest } from 'maplibre-gl-js-amplify';
+import { MapView } from "@aws-amplify/ui-react-geo";
+import { MapViewState } from "deck.gl/src";
+import { Button } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import "maplibre-gl/dist/maplibre-gl.css";
 
-import { MapView} from '@aws-amplify/ui-react-geo';
-import 'maplibre-gl/dist/maplibre-gl.css';
-
-const INITIAL_VIEW_STATE = {
+const INITIAL_VIEW_STATE: MapViewState = {
   //longitude: 139.7674681227469,
-  longitude: -80.20321, 
+  longitude: -80.20321,
   //latitude: 35.68111419325676,
-  latitude: 26.00068, 
+  latitude: 26.00068,
   zoom: 17,
   bearing: 0,
   pitch: 0,
 };
-//const client = generateClient<Schema>();
 
 function App() {
-  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const { signOut } = useAuthenticator();
-  //const [viewport, setViewport] = useState(INITIAL_VIEW_STATE);
-
-  
-  // useEffect(() => {
-  //   client.models.Todo.observeQuery().subscribe({
-  //     next: (data) => setTodos([...data.items]),
-  //   });
-  // }, []);
-
-  // function createTodo() {
-  //   client.models.Todo.create({ content: window.prompt("Todo content") });
-  // }
-
-  // function deleteTodo(id: string) {
-  //   client.models.Todo.delete({ id });
-  // }
+  const [viewport, setViewport] = useState<MapViewState>(INITIAL_VIEW_STATE);
+  const layers:any = [];
 
   return (
     <main>
+      <>
       
-        <>
-        <MapView initialViewState={INITIAL_VIEW_STATE} style={{
-          position: 'absolute',
-          zIndex: -1,
-        }}/>;
-      <button onClick={signOut}>Sign out</button>
-        </>
-      
-      
+        <DeckGL
+          initialViewState={INITIAL_VIEW_STATE}
+          controller
+          layers={layers}
+          onViewStateChange={({ viewState }: { viewState: MapViewState }) =>
+            setViewport(viewState)
+          }
+        >
+          <Button onClick={signOut}>Sign out</Button>
+          <MapView
+            {...viewport}
+            initialViewState={INITIAL_VIEW_STATE}
+            style={{
+              position: "absolute",
+              zIndex: -1,
+            }}
+          />
+          
+        </DeckGL>
+        
+      </>
     </main>
   );
 }

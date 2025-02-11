6bbae541d15e52c88ede14f19db87ed98a2657fd
Xin Qiao <qiaoxin136@gmail.com>
2025-02-11 13:23:56 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { DeckGL } from "@deck.gl/core";
+import { DeckGL } from "deck.gl";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { MapView } from "@aws-amplify/ui-react-geo";
-import { MapViewState } from "@deck.gl/core";
+import { MapViewState } from "deck.gl";
 import { Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import "maplibre-gl/dist/maplibre-gl.css";
